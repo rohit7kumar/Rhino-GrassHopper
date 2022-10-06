92599fae287b62108cdd7f44d883d83e14efada7
Admin <Admin@DESKTOP-NFT6VML>
2022-10-06 17:38:41 +0530

    Add project files.

---
+++ MathComponent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MyFirstGrassHopperPlugin
+{
+    public class MathComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MathComponent class.
+        /// </summary>
+        public MathComponent()
+          : base("AddNumbers", "AddNum",
+              "Add two numbers together",
+              "MyFirstGrassHopper", "Operators")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Angle", "A", "The angle to measure", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Radians", "R", "Work in Radians", GH_ParamAccess.item, true); // The default value is 'true'
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Sin", "sin", "The sine of the Angle.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Cos", "cos", "The cosine of the Angle.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Tan", "tan", "The tangent of the Angle.", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Declare variables to contain all inputs.
+            // We can assign some initial values that are either sensible or indicative.
+            double angle = double.NaN;
+            bool radians = false;
+
+            // Use the DA object to retrieve the data inside the input parameters.
+            // If the retrieval fails (for example if there is no data) we need to abort.
+            if (!DA.GetData(0, ref angle)) { return; }
+            if (!DA.GetData(1, ref radians)) { return; }
+
+            // If the angle value is not a valid number, we should abort.
+            if (!Rhino.RhinoMath.IsValidDouble(angle)) { return; }
+
+            // If the user wants to work in degrees rather than radians, 
+            // we assume that angle is defined in degrees. 
+            // We need to convert it into Radians again.
+            if (!radians)
+            {
+                angle = Rhino.RhinoMath.ToRadians(angle);
+            }
+
+            // Now we are ready to assign the outputs via the DA object.
+            // Since the Sin(), Cos() and Tan() never fail, we might as well 
+            // combine them with the assignment.
+            DA.SetData(0, Math.Sin(angle));
+            DA.SetData(1, Math.Cos(angle));
+            DA.SetData(2, Math.Tan(angle));
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("551EF226-5EBB-4450-8EA1-2ED514E2A8F1"); }
+        }
+    }
+}
---
+++ MyFirstGrassHopperPluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace MyFirstGrassHopperPlugin
+{
+    public class MyFirstGrassHopperPluginInfo : GH_AssemblyInfo
+    {
+        public override string Name => "MyFirstGrassHopperPlugin";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("5A905CD6-DF26-4036-BDB3-A9A7BC2EFE38");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
